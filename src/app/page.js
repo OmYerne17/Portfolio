ae12d6ee204aa72f66fa9f2b919eb6977590642f
OmYerne17 <omyerane2004@gmail.com>
2025-01-31 12:17:13 +0530

    Build Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Header from "@/components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
@@ -6,7 +7,9 @@ import Skills from "../components/Skills";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import BackToTop from "../components/BackToTop";
-import '@/css/style.css'
+import '@/css/style.css';
+
+
 
 export default function Home() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,42 @@
-"use client"
-import React, { useEffect, useState } from 'react';
+import { jsx as _jsx, jsxs as _jsxs } from 'react/jsx-runtime';
 
 export default function Header() {
-    const [isVisible, setIsVisible] = useState(true);
-    let lastScrollY = window.scrollY;
+ 
 
-    const handleScroll = () => {
-        if (window.scrollY > lastScrollY) {
-            setIsVisible(false); // Scrolling down
-        } else {
-            setIsVisible(true); // Scrolling up
-        }
-        lastScrollY = window.scrollY;
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    return (
-      <header className={`header ${isVisible ? 'visible' : 'hidden'}`} data-header>
-        <div className="container">
+  return (
+    <header className="header  data-header">
+      <div className="container">
         <a href="#">
-            <h1 className="logo !text-[32px] font-bold list-disc hover:text-transparent bg-clip-text bg-gradient-to-r from-orange-900 to-orange-300">CodeWithOm.Com</h1>
-          </a>
-          <button className="nav-toggle-btn" aria-label="Toggle Menu" data-nav-toggle-btn>
-            <ion-icon name="menu-outline" className="menu-icon"></ion-icon>
-            <ion-icon name="close-outline" className="close-icon"></ion-icon>
-          </button>
-          <nav className="navbar container">
-            <ul className="navbar-list">
-              <li>
-                <a href="#home" className="navbar-link" data-nav-link>Home</a>
-              </li>
-              <li>
-                <a href="#about" className="navbar-link" data-nav-link>About</a>
-              </li>
-              <li>
-                <a href="#portfolio" className="navbar-link" data-nav-link>Portfolio</a>
-              </li>
-              <li>
-                <a href="#skills" className="navbar-link" data-nav-link>Skills</a>
-              </li>
-              <li>
-                <a href="#contact" className="navbar-link" data-nav-link>Contact</a>
-              </li>
-              <li>
-                <a href="https://drive.google.com/file/d/1VhtXIAseO14F4LNRFFISUeVjo-eYnbZr/view?usp=sharing" className="btn btn-primary">Download CV</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
-  } 
+          <h1 className="logo !text-[32px] font-bold list-disc hover:text-transparent bg-clip-text bg-gradient-to-r from-orange-900 to-orange-300">
+            CodeWithOm.Com
+          </h1>
+        </a>
+        <button
+          className="nav-toggle-btn"
+          aria-label="Toggle Menu"
+          data-nav-toggle-btn
+        >
+          <ion-icon name="menu-outline" className="menu-icon"></ion-icon>
+          <ion-icon name="close-outline" className="close-icon"></ion-icon>
+        </button>
+        <nav className="navbar container">
+          <ul className="navbar-list">
+            <li><a href="#home" className="navbar-link" data-nav-link>Home</a></li>
+            <li><a href="#about" className="navbar-link" data-nav-link>About</a></li>
+            <li><a href="#portfolio" className="navbar-link" data-nav-link>Portfolio</a></li>
+            <li><a href="#skills" className="navbar-link" data-nav-link>Skills</a></li>
+            <li><a href="#contact" className="navbar-link" data-nav-link>Contact</a></li>
+            <li>
+              <a
+                href="https://drive.google.com/file/d/1VhtXIAseO14F4LNRFFISUeVjo-eYnbZr/view?usp=sharing"
+                className="btn btn-primary"
+              >
+                Download CV
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
